0d02016357024df11c98e065c1819f4dcb3d63ce
Aniket Pawar <aniket@paradox.ai>
2026-03-12 09:47:04 +0530

    build: add production build for GitHub Pages

---
+++ dist/assets/bot-BdqQSkpm.js
@@ -0,0 +1,22 @@
+import{r as o}from"./vendor-BWFb42Va.js";/**
+ * @license lucide-react v0.363.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var k={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};/**
+ * @license lucide-react v0.363.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase();/**
+ * @license lucide-react v0.363.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const w=(e,n)=>{const a=o.forwardRef(({color:c="currentColor",size:t=24,strokeWidth:s=2,absoluteStrokeWidth:h,className:d="",children:r,...i},u)=>o.createElement("svg",{ref:u,...k,width:t,height:t,stroke:c,strokeWidth:h?Number(s)*24/Number(t):s,className:["lucide",`lucide-${m(e)}`,d].join(" "),...i},[...n.map(([p,l])=>o.createElement(p,l)),...Array.isArray(r)?r:[r]]));return a.displayName=`${e}`,a};/**
+ * @license lucide-react v0.363.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const g=w("Bot",[["path",{d:"M12 8V4H8",key:"hb8ula"}],["rect",{width:"16",height:"12",x:"4",y:"8",rx:"2",key:"enze0r"}],["path",{d:"M2 14h2",key:"vft8re"}],["path",{d:"M20 14h2",key:"4cs60a"}],["path",{d:"M15 13v2",key:"1xurst"}],["path",{d:"M9 13v2",key:"rq6x2g"}]]);export{g as B,w as c};
+//# sourceMappingURL=bot-BdqQSkpm.js.map
